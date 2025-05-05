a29a867e64925e6bfd1cf387a8a33a1172d4a5a2
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2025-05-05 16:07:17 +0600

    create Refresh Token

---
+++ src/main/java/com/Sneaker1/entity/RefreshToken.java
@@ -0,0 +1,20 @@
+package com.Sneaker1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class RefreshToken {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String token;
+    private LocalDateTime expiryDate;
+
+    @ManyToOne
+    private User user;
+}
